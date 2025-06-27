2e50831afafba462bea596e393582f42ed0fa775
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-06-27 21:26:04 +0200

    add timer

---
+++ questions.js
@@ -0,0 +1,27 @@
+export const quizzCapital = [
+    {
+      texte: "Quelle est la capitale de l’Italie ?",
+      choix: ["Rome", "Milan", "Florence", "Naples", "Turin"],
+      bonneReponse: "Rome"
+    },
+    {
+      texte: "Quelle est la capitale du Canada ?",
+      choix: ["Toronto", "Ottawa", "Montréal", "Vancouver", "Québec"],
+      bonneReponse: "Ottawa"
+    },
+    {
+      texte: "Quelle est la capitale de la Turquie ?",
+      choix: ["Ankara", "Istanbul", "Izmir", "Antalya", "Bursa"],
+      bonneReponse: "Ankara"
+    },
+    {
+      texte: "Quelle est la capitale de l’Australie ?",
+      choix: ["Sydney", "Melbourne", "Brisbane", "Canberra", "Perth"],
+      bonneReponse: "Canberra"
+    },
+    {
+      texte: "Quelle est la capitale de la Thaïlande ?",
+      choix: ["Chiang Mai", "Phuket", "Pattaya", "Bangkok", "Ayutthaya"],
+      bonneReponse: "Bangkok"
+    }
+  ];
---
+++ script.js
@@ -0,0 +1,114 @@
+import { quizzCapital } from './questions.js';
+
+// je prends les élements html ci dessous
+const containerQuestion = document.querySelector(".container-question")
+const containerChoices = document.querySelector(".container-choices")
+const rejouerButton = document.querySelector("#rejouer")
+const timerDisplay = document.getElementById("timer");
+
+
+
+let indexQuestion = 0 //  Index de la question actuelle dans le tableau (0 = première question)
+let score = 0 // nombre de bonne réponse 
+let reponseSelectionner = false // indique si l'utilisateur a selectionner une reponse 
+let tempsRestant = 10 // compte à rebours
+let interValId = null; // pour stocker le timer
+
+
+const nextQuestion=()=>{
+setTimeout(() => {
+    if(indexQuestion<quizzCapital.length -1) {
+    indexQuestion+=1
+    showQuestion()
+    }
+    else{
+    containerQuestion.innerText=`Fin du quizz tu as obtenu ${score} bonnes réponses sur ${quizzCapital.length}`
+    containerChoices.innerHTML = ""
+    document.getElementById("timer").innerText=""
+    rejouerButton.style.display= "inline-block"
+    }
+}, 2000)
+}
+
+const showQuestion = () => {
+
+
+    containerQuestion.innerHTML = ""; // vide les questions avant de remplir la nvl qst
+    containerChoices.innerHTML = ""; // vide les questions avant de remplir la nvl qst
+    reponseSelectionner = false  // quand je passe a une nouvelle question je réinitialise la variable pour forcer une réponse
+
+
+    let questionActuelle = quizzCapital[indexQuestion] // je crée une variable qui prendra le tableau d'objet (valeur = 0 plus haut donc la question 0 )
+    let askHtml = document.createElement("p")  // je crée une balise p
+    askHtml.innerText = questionActuelle.texte // je modifie le contenu de ma balise p pour que ce sois la question 
+    containerQuestion.appendChild(askHtml) // la div container-question devient le parent de la div p donc la div p est dans la class container question
+
+    tempsRestant = 10 // remet le compteurr à zero a chaque debut de qst
+    clearInterval(interValId) // permet d'arrêter le compteur 
+    timerDisplay.innerText = tempsRestant // maj de l'affichage du timer
+    interValId = setInterval(() => {
+        tempsRestant -= 1
+        timerDisplay.innerText = tempsRestant
+        if(tempsRestant === 0){
+            clearInterval(interValId)
+            nextQuestion() 
+            const allButtons = containerChoices.querySelectorAll("button")
+                allButtons.forEach((button) => {
+                    button.disabled = true
+                    if(button.innerText === questionActuelle.bonneReponse){
+                        button.style.backgroundColor="green"
+                    }
+                })          
+        }
+    }, 1000)
+
+    questionActuelle.choix.forEach((choix) => { // Pour chaque choix(réponses dans le tableau = choix) fais quelque chose le code ci dessous
+        let choixButton = document.createElement("button") // créer un element bouton sur le html 
+        choixButton.innerText = choix // modifie le texte html par choix (cf le tableau choix = les réponses aux questions)
+        containerChoices.appendChild(choixButton) // container choices devient parent de choixbouton donc les boutons réponses font parties de la div container choices
+
+        choixButton.addEventListener("click", () => {
+            clearInterval(interValId)
+
+            const allButtons = containerChoices.querySelectorAll("button")//  Récupère tous les boutons de réponse actuellement affichés
+            
+             //nextButton.style.display = "block"
+            if (choix === questionActuelle.bonneReponse) {    // Si le texte du bouton cliqué est la bonne réponse
+                score += 1 // chaque fois que la bonne réponse est selectionné augmente de 1 le score
+                choixButton.style.backgroundColor = "green"// Met le bouton en vert
+                reponseSelectionner = true // reponse selectionner = true donc oui
+            }
+            else {
+                choixButton.style.backgroundColor = "red"//  Sinon, met le bouton cliqué en rouge
+                reponseSelectionner = true // reponse selectionner = true donc oui
+            }
+                // colorie la bonne réponse et desactive les boutons
+                allButtons.forEach((button) => {
+                    if (button.innerText === questionActuelle.bonneReponse) {
+                    button.style.backgroundColor = "green"
+                    }
+                    button.disabled = true
+                })
+                   nextQuestion()
+            })
+        })
+    }
+
+rejouerButton.addEventListener('click', () => { // si je clique sur le bouton rejouer lit le code ci dessous
+    indexQuestion = 0 // valeur redevient à zero
+    score = 0 // remet le score à zero
+    rejouerButton.style.display = "none" // bouton rejouer invisible
+    showQuestion() // relance les questions
+})
+
+showQuestion()
+
+
+
+
+
+
+
+
+
+
