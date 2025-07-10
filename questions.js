074900d0050f7a521f35531fc4c1a7cb2216eb2e
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-10 10:08:04 +0200

    10juillet modif

--- questions.js
+++ questions.js
@@ -1,32 +1,541 @@
 export const quizzCapital = [
-    {
-      texte: "Quelle est la capitale de l’Italie ?",
-      choix: ["Rome", "Milan", "Florence", "Naples", "Turin"],
-      bonneReponse: "Rome",
-      background :"url('./images/italie.jpg')"
-    },
-    {
-      texte: "Quelle est la capitale du Canada ?",
-      choix: ["Toronto", "Ottawa", "Montréal", "Vancouver", "Québec"],
-      bonneReponse: "Ottawa",
-      background :"url('./images/canada.jpg')"
-    },
-    {
-      texte: "Quelle est la capitale de la Turquie ?",
-      choix: ["Ankara", "Istanbul", "Izmir", "Antalya", "Bursa"],
-      bonneReponse: "Ankara",
-      background : "url('./images/turquie.jpg')"
-    },
-    {
-      texte: "Quelle est la capitale de l’Australie ?",
-      choix: ["Sydney", "Melbourne", "Brisbane", "Canberra", "Perth"],
-      bonneReponse: "Canberra",
-      background: "url('./images/australie.jpg')"
-    },
-    {
-      texte: "Quelle est la capitale de la Thaïlande ?",
-      choix: ["Chiang Mai", "Phuket", "Pattaya", "Bangkok", "Ayutthaya"],
-      bonneReponse: "Bangkok",
-      background: "url('./images/thailande.jpg')"
-    }
-  ];
+  {
+    texte: "Quelle est la capitale de Italie ?",
+    choix: [
+      "Rome",
+      "Florence",
+      "Milan",
+      "Naples"
+    ],
+    bonneReponse: "Rome",
+    background: "url('./images/italie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Canada ?",
+    choix: [
+      "Toronto",
+      "Montréal",
+      "Ottawa",
+      "Québec"
+    ],
+    bonneReponse: "Ottawa",
+    background: "url('./images/Canada.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Turquie ?",
+    choix: [
+      "Istanbul",
+      "Antalya",
+      "Bursa",
+      "Ankara"
+    ],
+    bonneReponse: "Ankara",
+    background: "url('./images/turquie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Australie ?",
+    choix: [
+      "Sydney",
+      "Canberra",
+      "Melbourne",
+      "Perth"
+    ],
+    bonneReponse: "Canberra",
+    background: "url('./images/australie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Thaïlande ?",
+    choix: [
+      "Ayutthaya",
+      "Bangkok",
+      "Chiang Mai",
+      "Pattaya"
+    ],
+    bonneReponse: "Bangkok",
+    background: "url('./images/thailande.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de France ?",
+    choix: [
+      "Lyon",
+      "Nice",
+      "Marseille",
+      "Paris"
+    ],
+    bonneReponse: "Paris",
+    background: "url('./images/france.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Allemagne ?",
+    choix: [
+      "Berlin",
+      "Munich",
+      "Stuttgart",
+      "Francfort"
+    ],
+    bonneReponse: "Berlin",
+    background: "url('./images/allemagne.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Espagne ?",
+    choix: [
+      "Barcelone",
+      "Séville",
+      "Madrid",
+      "Valence"
+    ],
+    bonneReponse: "Madrid",
+    background: "url('./images/espagne.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Portugal ?",
+    choix: [
+      "Porto",
+      "Braga",
+      "Lisbonne",
+      "Coimbra"
+    ],
+    bonneReponse: "Lisbonne",
+    background: "url('./images/portugal.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Grèce ?",
+    choix: [
+      "Athènes",
+      "Thessalonique",
+      "Rhodes",
+      "Corfou"
+    ],
+    bonneReponse: "Athènes",
+    background: "url('./images/grece.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Japon ?",
+    choix: [
+      "Kyoto",
+      "Hiroshima",
+      "Osaka",
+      "Tokyo"
+    ],
+    bonneReponse: "Tokyo",
+    background: "url('./images/japon.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Chine ?",
+    choix: [
+      "Shenzhen",
+      "Wuhan",
+      "Shanghai",
+      "Pékin"
+    ],
+    bonneReponse: "Pékin",
+    background: "url('./images/chine.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Inde ?",
+    choix: [
+      "Hyderabad",
+      "Bangalore",
+      "New Delhi",
+      "Chennai"
+    ],
+    bonneReponse: "New Delhi",
+    background: "url('./images/inde.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Brésil ?",
+    choix: [
+      "Brasília",
+      "São Paulo",
+      "Salvador",
+      "Rio"
+    ],
+    bonneReponse: "Brasília",
+    background: "url('./images/bresil.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Mexique ?",
+    choix: [
+      "Mexico",
+      "Guadalajara",
+      "Monterrey",
+      "Cancún"
+    ],
+    bonneReponse: "Mexico",
+    background: "url('./images/mexique.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Argentine ?",
+    choix: [
+      "Buenos Aires",
+      "Rosario",
+      "La Plata",
+      "Mendoza"
+    ],
+    bonneReponse: "Buenos Aires",
+    background: "url('./images/argentine.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Colombie ?",
+    choix: [
+      "Cartagena",
+      "Bogotá",
+      "Cali",
+      "Medellín"
+    ],
+    bonneReponse: "Bogotá",
+    background: "url('./images/colombie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Pérou ?",
+    choix: [
+      "Trujillo",
+      "Piura",
+      "Lima",
+      "Arequipa"
+    ],
+    bonneReponse: "Lima",
+    background: "url('./images/perou.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de États-Unis ?",
+    choix: [
+      "Los Angeles",
+      "Miami",
+      "New York",
+      "Washington"
+    ],
+    bonneReponse: "Washington",
+    background: "url('./images/etats-unis.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Royaume-Uni ?",
+    choix: [
+      "Birmingham",
+      "Londres",
+      "Bristol",
+      "Manchester"
+    ],
+    bonneReponse: "Londres",
+    background: "url('./images/royaume-uni.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Irlande ?",
+    choix: [
+      "Cork",
+      "Galway",
+      "Limerick",
+      "Dublin"
+    ],
+    bonneReponse: "Dublin",
+    background: "url('./images/irlande.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Norvège ?",
+    choix: [
+      "Tromsø",
+      "Trondheim",
+      "Bergen",
+      "Oslo"
+    ],
+    bonneReponse: "Oslo",
+    background: "url('./images/norvege.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Suède ?",
+    choix: [
+      "Gothenburg",
+      "Stockholm",
+      "Lund",
+      "Malmö"
+    ],
+    bonneReponse: "Stockholm",
+    background: "url('./images/suede.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Finlande ?",
+    choix: [
+      "Helsinki",
+      "Turku",
+      "Oulu",
+      "Espoo"
+    ],
+    bonneReponse: "Helsinki",
+    background: "url('./images/finlande.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Danemark ?",
+    choix: [
+      "Odense",
+      "Esbjerg",
+      "Copenhague",
+      "Aarhus"
+    ],
+    bonneReponse: "Copenhague",
+    background: "url('./images/danemark.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Suisse ?",
+    choix: [
+      "Bâle",
+      "Berne",
+      "Genève",
+      "Lausanne"
+    ],
+    bonneReponse: "Berne",
+    background: "url('./images/danemark.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Belgique ?",
+    choix: [
+      "Liège",
+      "Bruges",
+      "Anvers",
+      "Bruxelles"
+    ],
+    bonneReponse: "Bruxelles",
+    background: "url('./images/belgique.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Pays-Bas ?",
+    choix: [
+      "La Haye",
+      "Eindhoven",
+      "Utrecht",
+      "Amsterdam"
+    ],
+    bonneReponse: "Amsterdam",
+    background: "url('./images/pays-bas.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Autriche ?",
+    choix: [
+      "Vienne",
+      "Linz",
+      "Salzbourg",
+      "Graz"
+    ],
+    bonneReponse: "Vienne",
+    background: "url('./images/autriche.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Pologne ?",
+    choix: [
+      "Cracovie",
+      "Varsovie",
+      "Wrocław",
+      "Gdańsk"
+    ],
+    bonneReponse: "Varsovie",
+    background: "url('./images/pologne.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de République tchèque ?",
+    choix: [
+      "Liberec",
+      "Plzeň",
+      "Ostrava",
+      "Prague"
+    ],
+    bonneReponse: "Prague",
+    background: "url('./images/republique-tcheque.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Hongrie ?",
+    choix: [
+      "Debrecen",
+      "Budapest",
+      "Pécs",
+      "Szeged"
+    ],
+    bonneReponse: "Budapest",
+    background: "url('./images/hongrie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Roumanie ?",
+    choix: [
+      "Bucarest",
+      "Constanța",
+      "Iași",
+      "Timișoara"
+    ],
+    bonneReponse: "Bucarest",
+    background: "url('./images/roumanie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Serbie ?",
+    choix: [
+      "Niš",
+      "Subotica",
+      "Kragujevac",
+      "Belgrade"
+    ],
+    bonneReponse: "Belgrade",
+    background: "url('./images/serbie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Russie ?",
+    choix: [
+      "Saint-Pétersbourg",
+      "Novossibirsk",
+      "Moscou",
+      "Ekaterinbourg"
+    ],
+    bonneReponse: "Moscou",
+    background: "url('./images/russie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Ukraine ?",
+    choix: [
+      "Kharkiv",
+      "Dnipro",
+      "Lviv",
+      "Kyiv"
+    ],
+    bonneReponse: "Kyiv",
+    background: "url('./images/ukraine.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Corée du Sud ?",
+    choix: [
+      "Daejeon",
+      "Séoul",
+      "Daegu",
+      "Incheon"
+    ],
+    bonneReponse: "Séoul",
+    background: "url('./images/coree-du-sud.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Vietnam ?",
+    choix: [
+      "Hanoï",
+      "Huế",
+      "Hô Chi Minh-Ville",
+      "Can Tho"
+    ],
+    bonneReponse: "Hanoï",
+    background: "url('./images/vietnam.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Indonésie ?",
+    choix: [
+      "Yogyakarta",
+      "Bali",
+      "Jakarta",
+      "Bandung"
+    ],
+    bonneReponse: "Jakarta",
+    background: "url('./images/indonesie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Malaisie ?",
+    choix: [
+      "George Town",
+      "Malacca",
+      "Ipoh",
+      "Kuala Lumpur"
+    ],
+    bonneReponse: "Kuala Lumpur",
+    background: "url('./images/malaisie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Afrique du Sud ?",
+    choix: [
+      "Bloemfontein",
+      "Durban",
+      "Johannesburg",
+      "Pretoria"
+    ],
+    bonneReponse: "Pretoria",
+    background: "url('./images/afrique-du-sud.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Égypte ?",
+    choix: [
+      "Louxor",
+      "Gizeh",
+      "Alexandrie",
+      "Le Caire"
+    ],
+    bonneReponse: "Le Caire",
+    background: "url('./images/egypte.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Maroc ?",
+    choix: [
+      "Tanger",
+      "Rabat",
+      "Casablanca",
+      "Fès"
+    ],
+    bonneReponse: "Rabat",
+    background: "url('./images/maroc.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Algérie ?",
+    choix: [
+      "Constantine",
+      "Alger",
+      "Blida",
+      "Annaba"
+    ],
+    bonneReponse: "Alger",
+    background: "url('./images/algerie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Tunisie ?",
+    choix: [
+      "Sousse",
+      "Gabès",
+      "Sfax",
+      "Tunis"
+    ],
+    bonneReponse: "Tunis",
+    background: "url('./images/tunisie.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Nigeria ?",
+    choix: [
+      "Port Harcourt",
+      "Abuja",
+      "Kano",
+      "Ibadan"
+    ],
+    bonneReponse: "Abuja",
+    background: "url('./images/nigeria.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Kenya ?",
+    choix: [
+      "Mombasa",
+      "Eldoret",
+      "Nakuru",
+      "Nairobi"
+    ],
+    bonneReponse: "Nairobi",
+    background: "url('./images/kenya.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Arabie saoudite ?",
+    choix: [
+      "Djeddah",
+      "La Mecque",
+      "Riyad",
+      "Médine"
+    ],
+    bonneReponse: "Riyad",
+    background: "url('./images/arabie-saoudite.jpg')"
+  },
+  {
+    texte: "Quelle est la capitale de Émirats arabes unis ?",
+    choix: [
+      "Dubaï",
+      "Abu Dhabi",
+      "Sharjah",
+      "Ajman"
+    ],
+    bonneReponse: "Abu Dhabi",
+    background: "url('./images/emirats-arabes-unis.jpg')"
+  }
+]
--- script.js
+++ script.js
@@ -14,101 +14,155 @@ let reponseSelectionner = false // indique si l'utilisateur a selectionner une r
 let tempsRestant = 10 // compte à rebours
 let interValId = null; // stock le timer
 
+    
 
-const nextQuestionWithoutButton=()=>{
-setTimeout(() => {
-    if(indexQuestion<quizzCapital.length -1) {
-    indexQuestion+=1
-    progressBar.classList.remove("danger-blink")
-    showQuestion()
-    }
-    else{
-    containerQuestion.innerText=`Fin du quizz tu as obtenu ${score} bonnes réponses sur ${quizzCapital.length}`
-    containerChoices.innerHTML = ""
-    rejouerButton.style.display= "inline-block"
-    }
-}, 2000)
-}
 
 const showQuestion = () => {
-
-
-    containerQuestion.innerHTML = ""; // vide les questions avant de remplir la nvl qst
-    containerChoices.innerHTML = ""; // vide les questions avant de remplir la nvl qst
+    const questionActuelle = quizzCapital[indexQuestion] // je crée une variable qui prendra le tableau d'objet (valeur = 0 plus haut donc la question 0 )
     reponseSelectionner = false  // quand je passe a une nouvelle question je réinitialise la variable pour forcer une réponse
+    containerChoices.innerHTML = ""; // vide les questions avant de remplir la nvl qst
 
 
-    let questionActuelle = quizzCapital[indexQuestion] // je crée une variable qui prendra le tableau d'objet (valeur = 0 plus haut donc la question 0 )
-    document.body.style.backgroundImage = questionActuelle.background;
-    let askHtml = document.createElement("p")  // je crée une balise p
-    askHtml.innerText = questionActuelle.texte // je modifie le contenu de ma balise p pour que ce sois la question 
-    containerQuestion.appendChild(askHtml) // la div container-question devient le parent de la div p donc la div p est dans la class container question
+    changerBackground(questionActuelle.background)
+    afficherQuestionTexte(questionActuelle)
+    afficherChoix(questionActuelle)
+    resetTimer()
+    initialiserTimer(questionActuelle)
+}
 
-    tempsRestant = 10 // remet le compteurr à zero a chaque debut de qst
-    progressBar.style.width= "100%" // remet à 100% chaque qst
-    clearInterval(interValId) // permet d'arrêter le compteur 
+
+
+
+// reset timer à 10sec , arret de l'ancien interval timer, remettre la barre de progression au max , en vert, et enlever le clignotement
+function resetTimer() {
+    tempsRestant = 10
+    clearInterval(interValId)
+    progressBar.style.width= "100%"
+    progressBar.style.backgroundColor= "green"
+    progressBar.classList.remove("danger-blink"); 
+}
+
+// vide la qst precedente, insere la question et ajoute a la page
+function afficherQuestionTexte(questionActuelle){
+    containerQuestion.innerHTML = ""
+    let askHtml = document.createElement("p")
+    askHtml.innerText = questionActuelle.texte
+    containerQuestion.appendChild(askHtml)
+}
+
+// affiche qst suivante après 2sec
+const nextQuestionWithoutButton=()=>{
+  progressBar.style.backgroundColor="green"
+     progressBar.style.width= "100%"
+ setTimeout(() =>{
+     if(indexQuestion<quizzCapital.length -1) {
+     indexQuestion+=1
+     progressBar.classList.remove("danger-blink")
+     showQuestion()
+     }
+     else{
+     containerQuestion.innerText=`Fin du quizz tu as obtenu ${score} bonnes réponses sur ${quizzCapital.length}`
+     containerChoices.innerHTML = ""
+     rejouerButton.style.display= "inline-block"
+     }
+ }, 2000)
+ }
+ 
+ const homePage = () => {
+ 
+ }
+ 
+
+
+const afficherChoix=(questionActuelle)=>{
+    // parcourt chaque option de réponse à la question
+    questionActuelle.choix.forEach((choix) => { 
+      const choixButton = document.createElement("button")
+      choixButton.innerText = choix
+      containerChoices.appendChild(choixButton)
+  
+      choixButton.addEventListener("click", () => {
+        // stop le timer en cours
+        clearInterval(interValId) 
+        // recupere tout les boutons visibles
+
+        const allButtons = containerChoices.querySelectorAll("button")
+        
+        if (choix === questionActuelle.bonneReponse) {
+          score += 1
+          choixButton.style.backgroundColor = "green"
+          reponseSelectionner = true
+        } else {
+          choixButton.style.backgroundColor = "red"
+          reponseSelectionner = true
+        }
+        // parcourt tout les boutons pour 
+        allButtons.forEach((button) => {
+          if (button.innerText === questionActuelle.bonneReponse) {
+            // si on s'est trompé colorie la bonne reponse
+            button.style.backgroundColor = "green"  
+          }
+          // bloque les autres boutons
+          button.disabled = true
+        })
+        // question suivante avec un délai 2secondes cf
+        nextQuestionWithoutButton()
+      })
+    })
+  }
+
+
+const changerBackground= (background) => {
+    document.body.style.backgroundImage = background
+}
+
+
+function initialiserTimer(questionActuelle) {
     interValId = setInterval(() => {
         tempsRestant -= 1
-        if (tempsRestant > 5) {
-            progressBar.style.backgroundColor= "green"
-          } else if (tempsRestant > 2) {
-            progressBar.style.backgroundColor="orange"
-          } else if(tempsRestant<=2){
-            progressBar.style.backgroundColor= "Red"
-            progressBar.classList.add("danger-blink");     // Ajoute l’effet clignotement 
-          }
-      let pourcentage = (tempsRestant/10)*100  // % 10% par secondes
-        progressBar.style.width = `${pourcentage}%` // dynamique de la barre 
-        if(tempsRestant === 0){
+        majBarreProgression()
+
+        if (tempsRestant === 0) {
+            // stop le timer
             clearInterval(interValId)
-            nextQuestionWithoutButton() 
+            // recup tt les btns
             const allButtons = containerChoices.querySelectorAll("button")
-                allButtons.forEach((button) => {
-                    button.disabled = true
-                    if(button.innerText === questionActuelle.bonneReponse){
-                        button.style.backgroundColor="green"
-                    }
-                })          
-        }
-    }, 1000)
-
-    questionActuelle.choix.forEach((choix) => { // Pour chaque choix(réponses dans le tableau = choix) fais quelque chose le code ci dessous
-        let choixButton = document.createElement("button") // créer un element bouton sur le html 
-        choixButton.innerText = choix // modifie le texte html par choix (cf le tableau choix = les réponses aux questions)
-        containerChoices.appendChild(choixButton) // container choices devient parent de choixbouton donc les boutons réponses font parties de la div container choices
-
-        choixButton.addEventListener("click", () => {
-            clearInterval(interValId)
-
-            const allButtons = containerChoices.querySelectorAll("button")//  Récupère tous les boutons de réponse actuellement affichés
-            
-             //nextButton.style.display = "block"
-            if (choix === questionActuelle.bonneReponse) {    // Si le texte du bouton cliqué est la bonne réponse
-                score += 1 // chaque fois que la bonne réponse est selectionné augmente de 1 le score
-                choixButton.style.backgroundColor = "green"// Met le bouton en vert
-                reponseSelectionner = true // reponse selectionner = true donc oui
-            }
-            else {
-                choixButton.style.backgroundColor = "red"//  Sinon, met le bouton cliqué en rouge
-                reponseSelectionner = true // reponse selectionner = true donc oui
-            }
-                // colorie la bonne réponse et desactive les boutons
-                allButtons.forEach((button) => {
-                    if (button.innerText === questionActuelle.bonneReponse) {
-                    button.style.backgroundColor = "green"
-                    }
-                    button.disabled = true
-                })
-                   nextQuestionWithoutButton()
+            allButtons.forEach((button) => {
+              button.disabled = true
+              if (button.innerText === questionActuelle.bonneReponse) {
+                button.style.backgroundColor = "green"
+              }
             })
-        })
-    }
+            nextQuestionWithoutButton()  
+          }
+    }, 1000);
+}
 
-    rejouerButton.addEventListener('click', () => { // si je clique sur le bouton rejouer lit le code ci dessous
+const majBarreProgression =() =>{
+    // Convertit le temps restant en pourcentage (10 = 100%)
+    const pourcentage = (tempsRestant / 10) * 100      
+    // Applique la nouvelle largeur
+    progressBar.style.width = `${pourcentage}%`          
+  
+  
+    if (tempsRestant > 5) {
+      progressBar.style.backgroundColor = "green"
+      progressBar.classList.remove("danger-blink")
+    } else if (tempsRestant > 2) {
+      progressBar.style.backgroundColor = "orange"
+      progressBar.classList.remove("danger-blink")
+    } else {
+      progressBar.style.backgroundColor = "red"
+      // Ajoute l’effet clignotement
+      progressBar.classList.add("danger-blink")
+    }
+  }
+
+
+
+  rejouerButton.addEventListener('click', () => { // si je clique sur le bouton rejouer lit le code ci dessous
     indexQuestion = 0 // valeur redevient à zero
     score = 0 // remet le score à zero
     rejouerButton.style.display = "none" // bouton rejouer invisible
-    showQuestion() // relance les questions
+    showQuestion()
 })
-
-showQuestion()
